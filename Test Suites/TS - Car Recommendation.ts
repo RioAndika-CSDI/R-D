3241e7540a667831926b4a13e70458cafce51706
Marvin Ariel Johannes <56905071+Marvin6262@users.noreply.github.com>
2024-01-09 14:34:57 +0700

    Update Test Suite

---
+++ Test Suites/TS - Car Recommendation.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS - Car Recommendation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a080ea20-6c39-4180-946e-b3b24a1181b9</testSuiteGuid>
+   <testCaseLink>
+      <guid>dc5446d6-8707-4c61-8d43-c4892d51b447</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Homepage/TC- Car Recommendation</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
