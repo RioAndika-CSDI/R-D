03054f2a455eb84f7b3137bc088128d10d9ae722
Ayu Anggraini <ayu.anggraini@seva.id>
2024-02-12 14:45:58 +0700

    add test suite cms

---
+++ Test Suites/CMS/TS - CMS.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS - CMS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e6fa3392-addc-4085-9fcc-369e2ed2738c</testSuiteGuid>
+</TestSuiteEntity>
