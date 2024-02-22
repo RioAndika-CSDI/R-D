5881612df58500d7c22b954ccf6b784a7ff7f0ed
Ayu Anggraini <ayu.anggraini@seva.id>
2024-02-22 15:00:16 +0700

    add test case homepage : H001, H002, H003, H004, H005, H006, H007, H008, H009, H010, H011

--- Object Repository/Page_SEVA - Beli Mobil Baru Dengan Cicilan _bf97c5/button_Nanti saja.rs
+++ Object Repository/Page_SEVA - Beli Mobil Baru Dengan Cicilan _bf97c5/button_Nanti saja.rs
@@ -6,12 +6,12 @@
    <elementGuidId>097b3138-0500-4531-b7c8-975831433d9a</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//div[2]/div/div[2]/div/div[2]/button</value>
-      </entry>
-      <entry>
          <key>CSS</key>
          <value>div.citySelectorModal_buttonGroup__GNTXa > button.button_secondary__rsPoQ.button_big__H6cBN.undefined</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[2]/div/div[2]/div/div[2]/button</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
@@ -22,7 +22,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>button</value>
-      <webElementGuid>77eacd4c-a64b-4d27-9020-e4185bdb6a14</webElementGuid>
+      <webElementGuid>2dc53bd4-e80f-498d-ae91-9da8d080ad28</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -30,7 +30,7 @@
       <name>class</name>
       <type>Main</type>
       <value>button_secondary__rsPoQ button_big__H6cBN undefined</value>
-      <webElementGuid>c4d79d3e-9630-4217-8365-4c9af6942984</webElementGuid>
+      <webElementGuid>09cb330b-ebf6-4d2c-bdb7-e639e55374c5</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -38,7 +38,7 @@
       <name>data-testid</name>
       <type>Main</type>
       <value>button-nanti-saja-city</value>
-      <webElementGuid>c6ab6e5c-5d4a-4940-98c8-9cae08cd3e14</webElementGuid>
+      <webElementGuid>c0826723-a1a7-40db-9891-2b8ceb999707</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -46,7 +46,7 @@
       <name>text</name>
       <type>Main</type>
       <value>Nanti saja</value>
-      <webElementGuid>bec82a02-62e2-41b1-9544-767f007bcb0f</webElementGuid>
+      <webElementGuid>eeca6fee-0a51-4285-85f6-a93c8c8cdd5d</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -54,7 +54,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>/html[1]/body[1]/div[5]/div[@class=&quot;ant-modal-root css-nllxry&quot;]/div[@class=&quot;ant-modal-wrap ant-modal-centered&quot;]/div[@class=&quot;ant-modal css-nllxry city-selector-custom-modal&quot;]/div[@class=&quot;ant-modal-content&quot;]/div[@class=&quot;ant-modal-body&quot;]/div[@class=&quot;citySelectorModal_buttonGroup__GNTXa&quot;]/button[@class=&quot;button_secondary__rsPoQ button_big__H6cBN undefined&quot;]</value>
-      <webElementGuid>fbdd9557-4f19-491c-93d7-c6b626437570</webElementGuid>
+      <webElementGuid>28516491-3a7c-4ce2-8089-f65288dc7caa</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -62,7 +62,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//div[2]/div/div[2]/div/div[2]/button</value>
-      <webElementGuid>e610d9a9-1405-4d85-ba92-34c39521f29b</webElementGuid>
+      <webElementGuid>6240b506-d955-4a77-8e04-a3ee8070d57b</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -70,6 +70,6 @@
       <name>xpath:customAttributes</name>
       <type>Main</type>
       <value>//button[(text() = 'Nanti saja' or . = 'Nanti saja')]</value>
-      <webElementGuid>9cc275fc-1098-4500-a2f7-3148e1cde898</webElementGuid>
+      <webElementGuid>f1ad84c6-9a30-439e-9eca-9ca7ab01fa4c</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
