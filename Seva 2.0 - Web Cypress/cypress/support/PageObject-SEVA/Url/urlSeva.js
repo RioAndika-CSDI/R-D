c5679179487871f0195cde5710cbd814ff5cb244
RioAndika-CSDI <rio.andika@berijalan.net>
2026-03-02 14:15:22 +0700

    update p2_6

--- Seva 2.0 - Web Cypress/cypress/support/PageObject-SEVA/Url/urlSeva.js
+++ Seva 2.0 - Web Cypress/cypress/support/PageObject-SEVA/Url/urlSeva.js
@@ -1,4 +1,4 @@
-import'cypress-xpath'
+import 'cypress-xpath'
 
 class urlSeva {
     url = 'https://www.seva.id/?utm_source=testing00&utm_campaign=testing00';
@@ -6,23 +6,41 @@ class urlSeva {
     urlBlog = 'https://www.seva.id/blog/pinjol-resmi-ojk-2026?utm_source=testing00&utm_campaign=testing00'
     urlAkunProfile = 'https://www.seva.id/akun/profil?utm_source=testing00&utm_campaign=testing00'
 
+    blockHeavyResources() {
+        cy.intercept('*/*gtm', { statusCode: 200, body: '' })
+        cy.intercept('*/*hotjar', { statusCode: 200, body: '' })
+        cy.intercept('*/*facebook', { statusCode: 200, body: '' })
+        cy.intercept('*/*analytics', { statusCode: 200, body: '' })
+        cy.intercept('*/*clarity', { statusCode: 200, body: '' })
+        cy.intercept('*/*tiktok', { statusCode: 200, body: '' })
+        cy.intercept('*/*ads', { statusCode: 200, body: '' })
+        cy.intercept('*/.mp4', { statusCode: 200, body: '' })
+        cy.intercept('*/.webm', { statusCode: 200, body: '' })
+    }
+
     visitUrl() {
-        cy.visit(this.url);
+        this.blockHeavyResources()
+        cy.visit(this.url, { timeout: 180000 })
         cy.viewport(1920, 1080)
     }
 
     visitUrlPromo() {
-        cy.visit(this.urlPromo);
+        this.blockHeavyResources()
+        cy.visit(this.urlPromo, { timeout: 180000 })
+        cy.viewport(1920, 1080)
     }
 
     visitUrlBlog() {
-        cy.visit(this.urlBlog);
+        this.blockHeavyResources()
+        cy.visit(this.urlBlog, { timeout: 180000 })
+        cy.viewport(1920, 1080)
     }
 
     visitUrAkunProfile() {
-        cy.visit(this.urlAkunProfile);
+        this.blockHeavyResources()
+        cy.visit(this.urlAkunProfile, { timeout: 180000 })
+        cy.viewport(1920, 1080)
     }
-
 }
 
 export default new urlSeva();
